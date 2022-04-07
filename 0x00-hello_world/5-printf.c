2eb1db800089770dd97d215e8b1963ff625e9080
Chidimmaoranu <Chidimmaoranu>
2022-04-07 15:09:59 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Result: Always 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,<\n");
+result (0);
+}
